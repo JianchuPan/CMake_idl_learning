f6dfe63d8f402724b0e02cd742cfde72acdbc1bc
JianchuPan <285489754@qq.com>
2025-08-22 23:24:58 +0800

    Update Test02

--- Test02/HelloWorld.cpp
+++ Test02/HelloWorld.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <unitree/common/string_tool.hpp>
+#include <unitree/common/time/time_tool.hpp>
+using namespace unitree::common;
+// using namespace unitree;
 
 int main() {
+    int32_t xt = Compare("Hello", "World");
+    std::cout<< xt << std::endl;
+    time_t currentTime = GetCurrentTimeNanosecond();
+    std::cout << "Current time: " << currentTime << std::endl;
     std::cout<< "Hello, World 02!" << std::endl;
     return 0;
 }
