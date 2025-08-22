8a2884f82f0db17566e5394c46a751473a51347b
JianchuPan <285489754@qq.com>
2025-08-23 00:55:00 +0800

    add Test04:do idl2hpp in bash through idlc -l c++ ./HelloWorldData.idl

---
+++ Test04/publisher.cpp
@@ -0,0 +1,26 @@
+#include <unitree/robot/channel/channel_publisher.hpp>
+#include <unitree/common/time/time_tool.hpp>
+#include "HelloWorldData.hpp"
+
+#define TOPIC "TopicHelloWorld"
+
+using namespace unitree::robot;
+using namespace unitree::common;
+
+int main()
+{
+    std::cout << "Publisher1 started." << std::endl;
+    ChannelFactory::Instance()->Init(0);
+    ChannelPublisher<HelloWorldData::Msg> publisher(TOPIC);
+
+    publisher.InitChannel();
+
+    while (true)
+    {
+        HelloWorldData::Msg msg(unitree::common::GetCurrentTimeMillisecond(), "HelloWorld.");
+        publisher.Write(msg);
+        sleep(1);
+    }
+
+    return 0;
+}
